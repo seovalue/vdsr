16ac168d0475969800f9d670c9bb1c96c4206ce0
서민정 <mathmjseo@khu.ac.kr>
2021-05-23 21:24:17 +0900

    refactor: eval.py feature에 맞게 수정

---
+++ crop_feature.py
@@ -0,0 +1,23 @@
+def crop_feature(datapath, feature_type, scale_factor):
+  data_path = datapath
+  datatype = feature_type
+  rescale_factor = scale_factor
+  if not os.path.exists(data_path):
+      raise Exception(f"[!] {data_path} not existed")
+
+  hr_imgs = []
+  w, h = Image.open(datapath).size
+  width = int(w / 16)
+  height = int(h / 16)
+  lwidth = int(width / rescale_factor)
+  lheight = int(height / rescale_factor)
+  print("lr: ({} {}), hr: ({} {})".format(lwidth, lheight, width, height))
+  hr_image = Image.open(datapath)  # .convert('RGB')\
+  for i in range(16):
+      for j in range(16):
+          (left, upper, right, lower) = (
+          i * width, j * height, (i + 1) * width, (j + 1) * height)
+          crop = hr_image.crop((left, upper, right, lower))
+          hr_imgs.append(crop)
+      
+  return hr_imgs
--- eval.py
+++ eval.py
@@ -4,12 +4,17 @@ from torch.autograd import Variable
 import numpy as np
 import time, math, glob
 import scipy.io as sio
+from crop_feature import crop_feature
+from PIL import Image
 
 parser = argparse.ArgumentParser(description="PyTorch VDSR Eval")
 parser.add_argument("--cuda", action="store_true", help="use cuda?")
 parser.add_argument("--model", default="model/model_epoch_50.pth", type=str, help="model path")
 parser.add_argument("--dataset", default="Set5", type=str, help="dataset name, Default: Set5")
 parser.add_argument("--gpus", default="0", type=str, help="gpu ids (default: 0)")
+parser.add_argument("--featureType", default="p3", type=str)
+parser.add_argument("--scaleFactor", default=4, type=int, help="scale factor")
+
 
 def PSNR(pred, gt, shave_border=0):
     height, width = pred.shape[:2]
@@ -35,7 +40,7 @@ model = torch.load(opt.model, map_location=lambda storage, loc: storage)["model"
 scales = [4]
 
 # image_list = glob.glob(opt.dataset+"/*.*") 
-image_list = opt.dataset
+image_list = crop_feature(opt.dataset, opt.featureType, opt.scaleFactor)
 
 for scale in scales:
     avg_psnr_predicted = 0.0
@@ -43,44 +48,46 @@ for scale in scales:
     avg_elapsed_time = 0.0
     count = 0.0
     for image_name in image_list:
-        if str(scale) in image_name:
-            count += 1
-            print("Processing ", image_name)
-            im_gt_y = sio.loadmat(image_name)['im_gt_y']
-            im_b_y = sio.loadmat(image_name)['im_b_y']
-                       
-            im_gt_y = im_gt_y.astype(float)
-            im_b_y = im_b_y.astype(float)
+        count += 1
+        print("Processing ", image_name)
+        f_gt = image_name
+        w, h = image_name.size 
+        f_bi = image_name.resize((w//scale,h//scale), Image.BICUBIC)
+        f_bi = f_bi.resize((w,h), Image.BICUBIC)
 
-            psnr_bicubic = PSNR(im_gt_y, im_b_y,shave_border=scale)
-            avg_psnr_bicubic += psnr_bicubic
+        f_gt = np.array(f_gt)
+        f_bi = np.array(f_bi)    
+        f_gt = f_gt.astype(float)
+        f_bi = f_bi.astype(float)
 
-            im_input = im_b_y/255.
+        psnr_bicubic = PSNR(f_gt, f_bi,shave_border=scale)
+        avg_psnr_bicubic += psnr_bicubic
 
-            im_input = Variable(torch.from_numpy(im_input).float()).view(1, -1, im_input.shape[0], im_input.shape[1])
+        f_input = f_bi/255.
+        f_input = Variable(torch.from_numpy(f_input).float()).view(1, -1, f_input.shape[0], f_input.shape[1])
 
-            if cuda:
-                model = model.cuda()
-                im_input = im_input.cuda()
-            else:
-                model = model.cpu()
+        if cuda:
+            model = model.cuda()
+            im_input = im_input.cuda()
+        else:
+            model = model.cpu()
 
-            start_time = time.time()
-            HR = model(im_input)
-            elapsed_time = time.time() - start_time
-            avg_elapsed_time += elapsed_time
+        start_time = time.time()
+        SR = model(f_input)
+        elapsed_time = time.time() - start_time
+        avg_elapsed_time += elapsed_time
 
-            HR = HR.cpu()
+        SR = SR.cpu()
 
-            im_h_y = HR.data[0].numpy().astype(np.float32)
+        f_sr = SR.data[0].numpy().astype(np.float32)
 
-            im_h_y = im_h_y * 255.
-            im_h_y[im_h_y < 0] = 0
-            im_h_y[im_h_y > 255.] = 255.
-            im_h_y = im_h_y[0,:,:]
+        f_sr = f_sr * 255
+        f_sr[f_sr<0] = 0
+        f_sr[f_sr>255.] = 255.
+        f_sr = f_sr[0,:,:]
 
-            psnr_predicted = PSNR(im_gt_y, im_h_y,shave_border=scale)
-            avg_psnr_predicted += psnr_predicted
+        psnr_predicted = PSNR(f_gt, f_sr,shave_border=scale)
+        avg_psnr_predicted += psnr_predicted
 
     print("Scale=", scale)
     print("Dataset=", opt.dataset)
