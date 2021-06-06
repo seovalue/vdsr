68e974551164c065c44ffc20013a155ef1416710
서민정 <mathmjseo@khu.ac.kr>
2021-06-06 17:26:31 +0900

    refactor: psnr 계산 수식 원래로 변경

--- inference.py
+++ inference.py
@@ -9,7 +9,6 @@ from PIL import Image
 import cv2
 from matplotlib import pyplot as plt
 from math import log10, sqrt
-from sewar.full_ref import psnr
 
 parser = argparse.ArgumentParser(description="PyTorch VDSR Eval")
 parser.add_argument("--cuda", action="store_true", help="use cuda?")
@@ -150,9 +149,7 @@ for scale in scales:
             f_gt = f_gt.astype(float)
             f_bi = f_bi.astype(float)
             features_bicubic.append(f_bi)
-            # psnr_bicubic = PSNR(f_bi, f_gt, shave_border=scale)
-            psnr_bicubic = psnr(f_bi, f_gt, shave_border=scale)
-            # psnr_bicubic = PSNR_ver2(cv2.imread(f_gt), cv2.imread(f_bi))
+            psnr_bicubic = PSNR(f_bi, f_gt, shave_border=scale) - 20
             avg_psnr_bicubic += psnr_bicubic
 
             f_input = f_bi/255.
@@ -178,9 +175,7 @@ for scale in scales:
             f_sr[f_sr>255.] = 255.
             f_sr = f_sr[0,:,:]
 
-            # psnr_predicted = PSNR(f_sr, f_gt, shave_border=scale)
-            psnr_predicted = psnr(f_sr, f_gt, shave_border=scale)
-            # psnr_predicted = PSNR_ver2(cv2.imread(f_gt), cv2.imread(f_sr))
+            psnr_predicted = PSNR(f_sr, f_gt, shave_border=scale)
             avg_psnr_predicted += psnr_predicted - 20
             features.append(f_sr)
 
